195b456dceff795916440862b17ed3c46ab446aa
keertem1911 <keertem1911@126.com>
2018-12-11 17:34:52 +0800

    http axiso

--- src/common/httputil.js
+++ src/common/httputil.js
@@ -1,40 +1,17 @@
 import $ from 'axios'
-$.defaults.baseURL = 'http://localhost:8080/mmall';
+const baseURL = '/api';
 $.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-const http={
-};
- http.get =(url,param,callback)=>{
-  return $.get(url,{
-    params:param
-  }).then(res=>{
-    return res.data;
-  }).catch(error=>{
-    callback(error.message);
-    return''
-  })
-};
- http.post =(url,param,callback)=>{
-  return $.post(url,param).then(res=>{
-    return res.data;
-  }).catch(error=>{
-    callback(error.message);
-    return''
-  })
-};
- http.delete =(url,param,callback)=>{
-  return $.delete(url,param).then(res=>{
-    return res.data;
-  }).catch(error=>{
-    callback(error.message);
-    return''
-  })
-}; http.put =(url,param,callback)=>{
-  return $.put(url,param).then(res=>{
+export default function ajax(url,data={},type='GET'){
+  return new Promise(function (resolve, reject) {
+      if(type=== 'GET'){
+        var query="";
+        data.forEach(k=>{
+            query+= k+data[k]
+        })
+      }else{
 
-    return res.data;
-  }).catch(error=>{
-    callback(error.message);
-    return''
+      }
+
   })
-};
-export default http;
+}
+
